0dcbc4dba846646447a4270e43c4b58783208dae
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-07-06 17:22:11 +0530

    routes/index_updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { myContract, account } from '../instance.js';
 const router = Router();
 
 /* GET home page. */
@@ -23,6 +24,35 @@ router.get('/issue', function (req, res) {
     messageClass: 'hidden',
     certificateID: '<ID>',
   });
+});
+
+router.post('/issue', async function (req, res) {
+  let data = req.body;
+  console.log(data);
+
+  const txn = await myContract.methods
+    .issue(data.certificateID, data.name, data.course, data.grade, data.date)
+    .send({ from: account, gasLimit: '927000' });
+  console.log(txn);
+
+  res.render('issueCertificate', {
+    formClass: 'hidden',
+    messageClass: '',
+    certificateID: data.certificateID,
+  });
+});
+
+router.post('/view', async function (req, res) {
+  let data = req.body;
+  console.log(data);
+
+  const result = await myContract.methods
+    .Certificates(data.certificateID)
+    .call();
+  result.certificateID = data.certificateID;
+  console.log(result);
+
+  res.render('viewCertificate', { data: result });
 });
 
 export default router;
